b262c513eba718b3fe9ab499a6efbf8c9af12f79
EvanescenT07 <zulfikar.ahmad12@yahoo.com>
2023-12-09 10:44:16 +0700

    Add Change Language Feature and several update

--- app/src/main/java/com/example/pucanteen/authentication/LanguageActivity.java
+++ app/src/main/java/com/example/pucanteen/authentication/LanguageActivity.java
@@ -1,16 +1,60 @@
 package com.example.pucanteen.authentication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import com.example.pucanteen.MainActivity;
 import com.example.pucanteen.R;
 
 public class LanguageActivity extends AppCompatActivity {
+    CardView indonesianLang, englishLang;
+    ImageView backBtn;
+    private static String selectedLanguage = "en";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language);
+
+        indonesianLang = findViewById(R.id.indonesiaLanguage);
+        indonesianLang.setOnClickListener(v -> setLocale("id"));
+
+        englishLang = findViewById(R.id.englishLanguage);
+        englishLang.setOnClickListener(v -> setLocale("en"));
+
+        backBtn = findViewById(R.id.backButton);
+        backBtn.setOnClickListener(v -> {
+            Intent setBack = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(setBack);
+        });
+    }
+
+    private void setLocale(String languageCode) {
+        selectedLanguage = languageCode;
+        updateUIText();
+
+        // Use LocaleHelper to set the language
+        Context updatedContext = LocaleHelper.setLocale(this, languageCode);
+        recreateWithUpdatedContext(updatedContext);
+    }
+
+    private void recreateWithUpdatedContext(Context updatedContext) {
+        finish();
+        startActivity(new Intent(updatedContext, LanguageActivity.class)
+                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    private void updateUIText() {
+        CardView indonesiaCardView = findViewById(R.id.indonesiaLanguage);
+        CardView englishCardView = findViewById(R.id.englishLanguage);
+        TextView indonesiaTextView = indonesiaCardView.findViewById(R.id.indonesiaText);
+        TextView englishTextView = englishCardView.findViewById(R.id.englishText);
+        indonesiaTextView.setText(getString(R.string.indonesia));
+        englishTextView.setText(getString(R.string.english));
     }
 }
