8b172e7271f35fe88824e6dfaa26848f55b6cb5d
EvanescenT07 <zulfikar.ahmad12@yahoo.com>
2023-12-17 21:51:15 +0700

    Update Menu and solve minor error

---
+++ app/src/main/java/com/example/pucanteen/menu/Kebab.java
@@ -0,0 +1,16 @@
+package com.example.pucanteen.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.irfansaf.pucanteen.R;
+
+public class Kebab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kebab);
+    }
+}
---
+++ app/src/main/java/com/example/pucanteen/menu/ManinjauIndah.java
@@ -0,0 +1,16 @@
+package com.example.pucanteen.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.irfansaf.pucanteen.R;
+
+public class ManinjauIndah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maninjau_indah);
+    }
+}
